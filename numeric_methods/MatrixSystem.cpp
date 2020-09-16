ccb95771f6f42a8c1f1a5c425a737e12392e2d58
Wact <malygin.wm@gmail.com>
2020-09-16 23:26:27 +0700

    LDU works!

--- numeric_methods/MatrixSystem.cpp
+++ numeric_methods/MatrixSystem.cpp
@@ -53,37 +53,54 @@ void MatrixSystem<T>::ldu()
       T sum = 0;
       // number of items for i-th row/column
       const int count = ia[i + 1] - ia[i];
-      // index of current element in 'al' and 'au'
-      int k = ia[i] - 1;
 
-      for (int j = i - count; j < i; ++j)
+      // k - index of current element in 'al' and 'au'
+      for (int k = ia[i] - 1; k < ia[i + 1] - 1; ++k)
       {
          // sums for j-th row/column
          T sum_al = 0;
          T sum_au = 0;
 
-         int i_row = ia[i + 1] - ia[i];
-         int j_row = ia[j + 1] - ia[j];
-         int min = j_row < i_row
-            ? j_row - (i - count)
-            : i_row - (i - count);
+         // kj - index of first element in a row for L
+         //                      and in a column for U
+         int kj = ia[i] - 1;
 
-         int ki = k - min;
-         int kd = k - min - 1 - (i - count);
-         for (int kj = k - min - 1; kj < k - min + j - 1 - (i - count); ++kj)
+         // ki - index of first element in a column for L
+         //                            and in a row for U
+         int ki = ia[k - kj] - 1 + i - count;
+
+         // index of first diagonal element
+         int kd = i - count;
+
+         // difference between numbers of elements
+         //    for L: num row's elems - num column's elems
+         //    for U: num column's elems - num row's elems
+         const int diff = (k + 1 - ia[i]) - 
+            (ia[k - kj + i - count + 1] - ia[k - kj + i - count]);
+
+         // if 'diff' is positive then increase indexes 'kj' and 'kd'
+         // else increase index 'ki'
+         if (diff > 0)
          {
-            sum_al += au[kj] * di[kd] * al[ki];
-            sum_au += al[kj] * di[kd] * au[ki];
-            ++kd;
+            kj += diff;
+            kd += diff;
+         }
+         else
+            ki += -diff;
+
+         for (kj; kj < k; ++kj)
+         {
+            sum_al += al[kj] * di[kd] * au[ki];
+            sum_au += au[kj] * di[kd] * al[ki];
             ++ki;
+            ++kd;
          }
 
-         if (di[j] == 0) throw std::runtime_error("Matrix cannot be factorized!");
+         if (di[kd] == 0) throw std::runtime_error("Matrix cannot be factorized!");
 
-         au[k] = (au[k] - sum_au) / di[j];
-         al[k] = (al[k] - sum_al) / di[j];
-         sum += al[k] * di[j] * au[k];
-         ++k;
+         au[k] = (au[k] - sum_au) / di[kd];
+         al[k] = (al[k] - sum_al) / di[kd];
+         sum += al[k] * di[kd] * au[k];
       }
 
       di[i] -= sum;      
